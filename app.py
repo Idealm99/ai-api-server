e0ff253ec4db652d5c0f99b69c963b3a7355f4ff
Idealm99 <112872986+Idealm99@users.noreply.github.com>
2025-02-22 01:48:53 +0000

    langChain

---
+++ app.py
@@ -0,0 +1,31 @@
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, SystemMessage
+
+load_dotenv()
+model = init_chat_model("gpt-4o-mini", model_provider="openai")
+
+
+
+messages = [
+    SystemMessage("Translate the following from English into Italian"),
+    HumanMessage("hi!"),
+]
+
+# print(model.invoke(messages))
+
+# for token in model.stream(messages):
+#     print(token.content, end="|")
+
+from langchain_core.prompts import ChatPromptTemplate
+
+system_template = "Translate the following from English into {language}"
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)
+
+prompt = prompt_template.invoke({"language": "Italian", "text": "hi!"})
+
+response = model.invoke(prompt)
+print(response.content)
